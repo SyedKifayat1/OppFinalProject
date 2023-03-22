a8974e899d3f2362141ce6b202498757a6d0af13
Kifayat <kifayatkhanofficial@gmail.com>
2023-03-22 17:56:35 +0500

    commit
    find some changes in menu head cases

--- Menu.h
+++ Menu.h
@@ -18,52 +18,118 @@ void Menus::Menu()
     {
         cout << "\n.........Welcome To Mall Mangement System.........\n\n";
         cout << "Select Your Status :\n"
-        "1)Head\n"
-        "2)Manager\n"
-        "3)Sub-Manager\n"
-        "0)Exit\n"
-        "Enter Your Choice :";
+                "1)Head\n"
+                "2)Manager\n"
+                "3)Sub-Manager\n"
+                "0)Exit\n"
+                "Enter Your Choice :";
         cin >> choice;
         switch (choice)
         {
         case 1:
         {
+
             while (ch == 0)
             {
-                cout << "\n.......Welcome Head Repository.......\n\n"
-                "1)Employee Details\n"
-                "2)Shop Details\n"
-                "3)Account Details\n"
-                "4)Parking Details\n"
-                "5)Maintenance Details\n"
-                "0)Exit\n"
-                "Your Choice :";
+                cout << "\n.......Welcome To Head Repository.......\n\n"
+                        "1)Manage Manager\n"
+                        "2)Mall Details\n"
+                        "0)Exit\n"
+                        "Your Choice :";
                 cin >> choice;
                 switch (choice)
                 {
                 case 1:
-                {   
+                {
+                    while (ch == 0)
+                    {
+                        cout << "\n.......Welcome To Head Sub Repository.......\n\n"
+                                "1)Add Manager\n"
+                                "2)Remove Manager\n"
+                                "3)Replace Manager\n"
+                                "0)Exit\n"
+                                "Your Choice :";
+                        cin >> choice;
+                        switch (choice)
+                        {
+                        case 1:
+                        {
+                            break;
+                        }
+                        case 2:
+                        {
+                            break;
+                        }
+                        case 3:
+                        {
+                            break;
+                        }
+                        case 0:
+                        {
+                            ch = 1;
+                        }
+                        default:
+                        {
+                            cout << "\nInvalid Choice!\n";
+                            break;
+                        }
+                        }
+                    }
+                    ch = 0;
                     break;
-                }   
+                }
                 case 2:
-                {                   
-                    break;
-                }
-                case 3:
-                {   
-                    break;
-                }
-                case 4:
                 {
-                    break;
-                }
-                case 5:
-                {
+                    while (ch == 0)
+                    {
+                        cout << "\n.......Welcome To Head Sub Repository.......\n\n"
+                                "1)Employee Details\n"
+                                "2)Shop Details\n"
+                                "3)Account Details\n"
+                                "4)Parking Details\n"
+                                "5)Maintenance Details\n"
+                                "0)Exit\n"
+                                "Your Choice :";
+                        cin >> choice;
+                        switch (choice)
+                        {
+                        case 1:
+                        {
+                            break;
+                        }
+                        case 2:
+                        {
+                            break;
+                        }
+                        case 3:
+                        {
+                            break;
+                        }
+                        case 4:
+                        {
+                            break;
+                        }
+                        case 5:
+                        {
+                            break;
+                        }
+                        case 0:
+                        {
+                            ch = 1;
+                        }
+                        default:
+                        {
+                            cout << "\nInvalid Choice!\n";
+                            break;
+                        }
+                        }
+                    }
+                    ch = 0;
                     break;
                 }
                 case 0:
                 {
-                    ch=1;
+                    ch = 1;
                 }
                 default:
                 {
@@ -77,166 +143,166 @@ void Menus::Menu()
         }
         case 2:
         {
-             while (ch == 0)
+            while (ch == 0)
             {
                 cout << "\n.......Welcome Manager Repository.......\n\n"
-                "1)Employee Details\n"
-                "2)Shop Details\n"
-                "3)Account Details\n"
-                "4)Parking Details\n"
-                "5)Maintenance Details\n"
-                "0)Exit\n"
-                "Your Choice :";
+                        "1)Employee Details\n"
+                        "2)Shop Details\n"
+                        "3)Account Details\n"
+                        "4)Parking Details\n"
+                        "5)Maintenance Details\n"
+                        "0)Exit\n"
+                        "Your Choice :";
                 cin >> choice;
                 switch (choice)
                 {
                 case 1:
                 {
-                    while(ch==0)
+                    while (ch == 0)
                     {
-                        cout<<"\n.........Welcome To Employee Details.......\n\n";
-                        cout<<"Select Option :\n1)Display\n2)New Entry\n3)Update\n4)Delete\n0)Exit\nYour Choice :";
-                        cin>>choice;
+                        cout << "\n.........Welcome To Employee Details.......\n\n";
+                        cout << "Select Option :\n1)Display\n2)New Entry\n3)Update\n4)Delete\n0)Exit\nYour Choice :";
+                        cin >> choice;
                         switch (choice)
                         {
                         case 1:
                         {
-                            while(ch==0)
+                            while (ch == 0)
                             {
-                                cout<<"\n........Select The Status Of Employee........\n\n";
-                                cout<<"Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
-                                cin>>choice;
-                                switch(choice)
+                                cout << "\n........Select The Status Of Employee........\n\n";
+                                cout << "Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
+                                cin >> choice;
+                                switch (choice)
                                 {
-                                    case 1:
-                                    {
-                                        break;
-                                    }
-                                    case 2:
-                                    {
-                                        break;
-                                    }
-                                    case 3:
-                                    {
-                                        break;
-                                    }
-                                    case 4:
-                                    {
-                                        break;
-                                    }
-                                    case 0:
-                                    {
-                                        ch=1;
-                                        break;
-                                    }
+                                case 1:
+                                {
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    break;
+                                }
+                                case 0:
+                                {
+                                    ch = 1;
+                                    break;
+                                }
                                 }
                             }
-                            ch=0;
+                            ch = 0;
                             break;
                         }
                         case 2:
                         {
-                            while(ch==0)
+                            while (ch == 0)
                             {
-                                cout<<"\n........Select The Status Of Employee........\n\n";
-                                cout<<"Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
-                                cin>>choice;
-                                switch(choice)
+                                cout << "\n........Select The Status Of Employee........\n\n";
+                                cout << "Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
+                                cin >> choice;
+                                switch (choice)
                                 {
-                                    case 1:
-                                    {
-                                        break;
-                                    }
-                                    case 2:
-                                    {
-                                        break;
-                                    }
-                                    case 3:
-                                    {
-                                        break;
-                                    }
-                                    case 4:
-                                    {
-                                        break;
-                                    }
-                                    case 0:
-                                    {
-                                        ch=1;
-                                        break;
-                                    }
+                                case 1:
+                                {
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    break;
+                                }
+                                case 0:
+                                {
+                                    ch = 1;
+                                    break;
+                                }
                                 }
                             }
-                            ch=0;
+                            ch = 0;
                             break;
                         }
                         case 3:
                         {
-                            while(ch==0)
+                            while (ch == 0)
                             {
-                                cout<<"\n........Select The Status Of Employee........\n\n";
-                                cout<<"Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
-                                cin>>choice;
-                                switch(choice)
+                                cout << "\n........Select The Status Of Employee........\n\n";
+                                cout << "Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
+                                cin >> choice;
+                                switch (choice)
                                 {
-                                    case 1:
-                                    {
-                                        break;
-                                    }
-                                    case 2:
-                                    {
-                                        break;
-                                    }
-                                    case 3:
-                                    {
-                                        break;
-                                    }
-                                    case 4:
-                                    {
-                                        break;
-                                    }
-                                    case 0:
-                                    {
-                                        ch=1;
-                                        break;
-                                    }
+                                case 1:
+                                {
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    break;
+                                }
+                                case 0:
+                                {
+                                    ch = 1;
+                                    break;
+                                }
                                 }
                             }
-                            ch=0;
+                            ch = 0;
                             break;
                         }
                         case 4:
                         {
-                            while(ch==0)
+                            while (ch == 0)
                             {
-                                cout<<"\n........Select The Status Of Employee........\n\n";
-                                cout<<"Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
-                                cin>>choice;
-                                switch(choice)
+                                cout << "\n........Select The Status Of Employee........\n\n";
+                                cout << "Select Your Choice :\n1)Security Guards\n2)Parking\n3)Cleaning Staf\n4)Sales Man\n0)Exit\nEnter Your Choice :";
+                                cin >> choice;
+                                switch (choice)
                                 {
-                                    case 1:
-                                    {
-                                        break;
-                                    }
-                                    case 2:
-                                    {
-                                        break;
-                                    }
-                                    case 3:
-                                    {
-                                        break;
-                                    }
-                                    case 4:
-                                    {
-                                        break;
-                                    }
-                                    case 0:
-                                    {
-                                        ch=1;
-                                        break;
-                                    }
+                                case 1:
+                                {
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    break;
+                                }
+                                case 4:
+                                {
+                                    break;
+                                }
+                                case 0:
+                                {
+                                    ch = 1;
+                                    break;
+                                }
                                 }
                             }
-                            ch=0;
+                            ch = 0;
                             break;
                         }
                         case 0:
@@ -247,7 +313,7 @@ void Menus::Menu()
                             break;
                         }
                     }
-                    ch=0;
+                    ch = 0;
                     break;
                 }
                 case 2:
@@ -268,7 +334,7 @@ void Menus::Menu()
                 }
                 case 0:
                 {
-                    ch=1;
+                    ch = 1;
                 }
                 default:
                 {
@@ -285,12 +351,12 @@ void Menus::Menu()
             while (ch == 0)
             {
                 cout << "\n........Welcome To Sub-Manager Repository........\n\n"
-                "Select Your Status :\n"
-                "1)Account Manager\n"
-                "2)Maintenance Manager\n"
-                "3)Parking Manager\n"
-                "0)Exit\n"
-                "Enter Your Choice :";
+                        "Select Your Status :\n"
+                        "1)Account Manager\n"
+                        "2)Maintenance Manager\n"
+                        "3)Parking Manager\n"
+                        "0)Exit\n"
+                        "Enter Your Choice :";
                 cin >> choice;
                 switch (choice)
                 {

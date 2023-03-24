09e00e7c00639859fb89a28bd3124017c95729cb
Kifayat <kifayatkhanofficial@gmail.com>
2023-03-24 05:46:25 +0500

    commit
    I have deleted the manager option from our project.

--- Menu.h
+++ Menu.h
@@ -19,8 +19,7 @@ void Menus::Menu()
         cout << "\n.........Welcome To Mall Mangement System.........\n\n";
         cout << "Select Your Status :\n"
                 "1)Head\n"
-                "2)Manager\n"
-                "3)Sub-Manager\n"
+                "2)Sub-Manager\n"
                 "0)Exit\n"
                 "Enter Your Choice :";
         cin >> choice;
@@ -389,127 +388,8 @@ void Menus::Menu()
             ch = 0;
             break;
         }
+
         case 2:
-        {
-            while (ch == 0)
-            {
-                cout << "\n.......Welcome Manager Repository.......\n\n"
-                        "1)Employee Details\n"
-                        "2)Shop Details\n"
-                        "3)Account Details\n"
-                        "4)Parking Details\n"
-                        "5)Maintenance Details\n"
-                        "6)Manage Sub-Managers\n"
-                        "0)Exit\n"
-                        "Your Choice :";
-                cin >> choice;
-                switch (choice)
-                {
-                case 1:
-                {
-                    while (ch == 0)
-                    {
-                        cout << ".........Welcome To Employee Details Repository..........";
-                        cout << "1)Display Employee Details\n"
-                                "2)Edit Employee Details\n"
-                                "0)Exit\n"
-                                "Your Choice :";
-                        cin >> choice;
-                        switch (choice)
-                        {
-                        case 1:
-                        {
-                            while (ch == 0)
-                            {
-                                cout << "\n.......Welcome To Employee Details Repository.......\n\n"
-                                        "1)Getting Data By ID No \n"
-                                        "2)Getting Data As A Whole\n"
-                                        "3)Getting Data With Department Name\n"
-                                        "0)Exit\n"
-                                        "Your Choice :";
-                                cin >> choice;
-                                switch (choice)
-                                {
-                                case 1:
-                                {
-                                    // wirte a code for getting data with ID No
-                                    break;
-                                }
-                                case 2:
-                                {
-                                    // write a code for getting data as a whole
-                                    break;
-                                }
-                                case 3:
-                                {
-                                    // write a code for getting data with Department name
-                                    break;
-                                }
-                                case 0:
-                                {
-                                    ch = 1;
-                                }
-                                default:
-                                {
-                                    cout << "\nInvalid Choice!\n";
-                                    break;
-                                }
-                                }
-                            }
-                            ch = 0;
-                            break;
-                        }
-                        case 2:
-                        {
-                            break;
-                        }
-                        case 0:
-                        {
-                            ch = 1;
-                            break;
-                        }
-                        default:
-                        {
-                            cout << "\nInvalid Choice!\n";
-                            break;
-                        }
-                        }
-                        ch = 0;
-                    }
-
-                    break;
-                }
-                case 2:
-                {
-                }
-                case 3:
-                {
-
-                    break;
-                }
-                case 4:
-                {
-                    break;
-                }
-                case 5:
-                {
-                    break;
-                }
-                case 0:
-                {
-                    ch = 1;
-                }
-                default:
-                {
-                    cout << "\nInvalid Choice!\n";
-                    break;
-                }
-                }
-            }
-            ch = 0;
-            break;
-        }
-        case 3:
         {
             while (ch == 0)
             {

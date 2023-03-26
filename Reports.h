4d3ace1ce758abb1db65d8e30bb09f4162ee117c
Murtazakhalid-BSCE22004 <112537271+Murtazakhalid-BSCE22004@users.noreply.github.com>
2023-03-26 16:49:30 +0500

    commit

---
+++ Reports.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class Report
+{
+
+public:
+    void Add_report(string filename)
+    {
+        ofstream outfile(filename + ".txt");
+        string in;
+        cout << "WRITE A REPORT \n";
+        getline(cin, in);
+        outfile << in;
+    }
+
+    void Delete_report(string filename)
+    {
+        if (remove((filename + ".txt").c_str()) != 0)
+            cout << "Error deleting file\n";
+        else
+            cout << "File successfully deleted\n";
+    }
+
+    void Edit_report(string filename)
+    {
+        ofstream outfile(filename + ".txt");
+        string new_content;
+        cout << "WRITE A NEW REPORT\n";
+        getline(cin, new_content);
+        outfile << new_content;
+
+        cout << "REPORT SUCCESSFULLY UPDATED\n";
+    }
+};
